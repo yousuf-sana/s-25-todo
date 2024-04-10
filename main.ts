b0e58a12bf2ac40e9eb931551f8b856874482f25
sana yousuf <sanayousuf1992@gmail.com>
2024-04-10 15:19:14 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,76 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todos: string[]=[];
+let condition = true;
+
+while(condition){
+let ans = await inquirer.prompt([
+    {
+name: "select",
+message: "select an operator",
+type: "list",
+choices:["add", "update", "view", "delete", "exit"],
+}
+]);
+if (ans.select === "add" ){
+
+    let addTodo = await inquirer.prompt({
+        name: "todo",
+        message: "would you like to add in list?",
+        type: "input",
+        validate: function(input) { 
+            if (input.trim() == "" ){
+                return " kindly add any task." 
+            }
+            return true;
+         }
+
+    })
+    if(addTodo.todo.trim() !== "" ){
+    todos.push(addTodo.todo);
+    todos.forEach(todo=> console.log(todo))
+    }
+}
+if (ans.select==="update"){
+    let updateTodo = await inquirer.prompt({
+        name : "todo",
+        message: "update items in the  list",
+        type: "list",
+        choices: todos.map(item=> item)
+    })
+    let addTodo= await inquirer.prompt({
+        name: "todo",
+        message: "would you like to add in the list?",
+        type: "input",
+        
+    })
+    let newTodo =todos.filter( val => val !== updateTodo.todo);
+    todos=[...newTodo,addTodo.todo];
+    todos.forEach(todo=> console.log(todo))
+
+}
+if (ans.select==="view"){
+    console.log(chalk.green.bgBlueBright("***** TO DO LIST *****")),
+    todos.forEach(todo=> console.log(todo))
+}
+if (ans.select ==="delete"){ 
+    let deleteTodo = await inquirer.prompt ({
+        name: "todo",
+        message: "select item to delete",
+        type: "list", 
+        choices: todos.map( item => item)
+    })
+    let newTodo = todos.filter(val => val !== deleteTodo.todo);
+    todos = [...newTodo];
+     todos.forEach(todo=> console.log(todo))
+
+}
+if (ans.select === "exit"){
+    console.log(" Bye Bye...");
+    condition = false;
+}
+
+}
+
